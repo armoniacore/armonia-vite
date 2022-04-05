1e76a4cc2ad014ed3fb738f10c840c1b46bdad63
lucacicada <100947658+lucacicada@users.noreply.github.com>
2022-04-05 19:54:08 +0200

    Update electron example

--- packages/playground/electron-vue/src-electron/index.ts
+++ packages/playground/electron-vue/src-electron/index.ts
@@ -1,41 +1,48 @@
 import { app, BrowserWindow } from 'electron'
 
+let mainWindow: BrowserWindow | undefined
+
 function createWindow() {
-  // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
+    // show: false,
     width: 800,
     height: 600,
+    useContentSize: true,
     webPreferences: {
-      preload: import.meta.env.ELECTRON_PRELOAD_URL,
-      nodeIntegration: true
+      contextIsolation: true,
+      preload: import.meta.env.ELECTRON_PRELOAD_URL
     }
   })
+
+  // mainWindow.webContents.on('did-finish-load', function () {
+  //   mainWindow.show()
+  // })
 
   if (import.meta.env.DEV) {
     mainWindow.loadURL(import.meta.env.ELECTRON_APP_URL)
     mainWindow.webContents.openDevTools()
   } else {
     mainWindow.loadFile(import.meta.env.ELECTRON_APP_URL)
+    mainWindow.webContents.on('devtools-opened', () => {
+      mainWindow.webContents.closeDevTools()
+    })
   }
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow()
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+app.whenReady().then(createWindow)
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow == null) {
+    createWindow()
   }
 })
