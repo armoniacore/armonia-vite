05befe17a5ee721477a0b103cce0b5e4857846fb
lucacicada <100947658+lucacicada@users.noreply.github.com>
2022-04-18 10:52:14 +0200

    refactor electron build

--- packages/armonia-vite/src/plugin-electron/build.ts
+++ packages/armonia-vite/src/plugin-electron/build.ts
@@ -1,288 +1,171 @@
 import fs from 'fs'
 import path from 'path'
-import type { InlineConfig } from 'vite'
-import { build as viteBuild, mergeConfig } from 'vite'
+import type { InlineConfig, Plugin } from 'vite'
+import { mergeConfig } from 'vite'
 
 import { trim } from '../common/trim'
+import type { ResolvedElectronOptions } from './index'
 
-function locate(root: string, locations: string[]) {
-  for (const location of locations) {
-    if (fs.existsSync(path.resolve(root, location))) {
-      return location
+async function normalizePackageJson(pkg: Record<string, any>, options?: ResolvedElectronOptions) {
+  pkg.main = options?.main
+
+  const pkgDeps: Record<string, string> = pkg.dependencies || {}
+  const pkgDevDeps: Record<string, string> = pkg.devDependencies || {}
+
+  const resolvedDependencies: Record<string, string> = {}
+
+  if (options?.dependencies) {
+    if (Array.isArray(options.dependencies)) {
+      for (const dep of options.dependencies) {
+        if (pkgDeps[dep]) {
+          resolvedDependencies[dep] = pkgDeps[dep] as string
+        }
+
+        if (pkgDevDeps[dep]) {
+          resolvedDependencies[dep] = pkgDevDeps[dep] as string
+        }
+
+        if (pkgDeps[dep]) {
+          resolvedDependencies[dep] = pkgDeps[dep] as string
+        }
+      }
+    } else {
+      Object.assign(resolvedDependencies, options.dependencies)
     }
   }
 
-  return false
-}
+  if (options?.excludeDependencies) {
+    const keys = Array.isArray(options.excludeDependencies) ? options.excludeDependencies : Object.keys(options.excludeDependencies)
 
-function findElectronMain(root: string) {
-  return (
-    locate(root, [
-      'electron/main.ts',
-      'electron/main.js',
-      'electron/index.ts',
-      'electron/index.js',
-      'electron/electron.ts',
-      'electron/electron.js',
-      //
-      'src-electron/main.ts',
-      'src-electron/main.js',
-      'src-electron/index.ts',
-      'src-electron/index.js',
-      'src-electron/electron.ts',
-      'src-electron/electron.js',
-      'src-electron/electron-main.ts',
-      'src-electron/electron-main.js',
-      //
-      'src/electron.ts',
-      'src/electron.js',
-      'src/electron-main.ts',
-      'src/electron-main.js'
-    ]) || 'src/electron.js'
-  )
-}
-
-function findElectronPreload(root: string) {
-  return locate(root, [
-    'electron/preload.ts',
-    'electron/preload.js',
-    //
-    'src-electron/preload.ts',
-    'src-electron/preload.js',
-    'src-electron/electron-preload.ts',
-    'src-electron/electron-preload.js',
-    //
-    'src/preload.ts',
-    'src/preload.js',
-    'src/electron-preload.ts',
-    'src/electron-preload.js'
-  ])
-}
-
-function resolveUrl(config: InlineConfig): string {
-  let url = config.build?.rollupOptions?.input
-  if (typeof url !== 'string' || !url.endsWith('.html')) {
-    url = 'index.html'
+    for (const dep of keys) {
+      delete resolvedDependencies[dep]
+    }
   }
 
-  return url
+  pkg.dependencies = resolvedDependencies
+
+  delete pkg.scripts
+
+  // HACK: this is wrong
+  delete pkg.dependencies.vue
+  delete pkg.dependencies['vue-router']
+  delete pkg.dependencies['vuex']
+  delete pkg.dependencies['react']
+  delete pkg.dependencies['redux']
+
+  const devElectron = pkg.devDependencies?.electron
+
+  delete pkg.devDependencies
+
+  // HACK: this is wrong, its a fix for electron-builder
+  if (devElectron) {
+    pkg.devDependencies = {}
+    pkg.devDependencies.electron = trim(devElectron, '^')
+  }
+
+  await options?.transformPackageJson?.call(undefined, pkg)
+}
+
+type TransformPackageJson = ResolvedElectronOptions['transformPackageJson']
+
+function emitPackageJsonPlugin(options: ResolvedElectronOptions, transformPackageJson: TransformPackageJson): Plugin {
+  const packageJsonId = './package.json'
+
+  return {
+    name: 'armonia:electron-resolve-package-json',
+
+    buildStart() {
+      this.addWatchFile(packageJsonId)
+    },
+
+    async generateBundle() {
+      // TODO: provide a way to resolve package.json manually
+      const resolved = await this.resolve(packageJsonId)
+
+      let pkg: Record<string, any> = {}
+
+      if (resolved) {
+        try {
+          // TODO: get rid of `fs`, as right now, this is not possible
+          if (fs.existsSync(resolved.id)) {
+            pkg = JSON.parse(fs.readFileSync(resolved.id, 'utf8'))
+          }
+        } catch (error) {
+          this.warn({
+            id: packageJsonId,
+            message: 'Could not parse package.json file'
+          })
+        }
+      }
+
+      await normalizePackageJson(pkg, options)
+
+      await transformPackageJson?.call(undefined, pkg)
+
+      // emit the file
+      this.emitFile({
+        type: 'asset',
+        name: 'package.json',
+        source: JSON.stringify(pkg, null, 2)
+      })
+    }
+  }
 }
 
 interface ElectronBuildConfig {
   url?: string
-  main?: string
-  preload?: string
 }
 
-interface ExtraOptions {
-  transformPackageJson?: (pkg: Record<string, any>) => void | Promise<void>
-}
-
-export async function buildElectron(config: InlineConfig, electronConfig?: ElectronBuildConfig, options?: ExtraOptions) {
-  const resolvedRoot = config.root ? path.resolve(config.root) : process.cwd()
-
-  const mainEntry = electronConfig?.main || findElectronMain(resolvedRoot)
-  const preloadEntry = electronConfig?.preload || findElectronPreload(resolvedRoot)
-
-  // NOTE: entryFileNames will dictate the name of this file
-  const mainName = `${path.parse(mainEntry).name}.js`
-
-  const appUrl = electronConfig?.url || resolveUrl(config)
-  const preloadUrl = preloadEntry ? `./${path.parse(preloadEntry).name}.js` : false
-
-  const external: string[] = []
-
-  if (fs.existsSync(path.resolve(resolvedRoot, 'package.json'))) {
-    const packageJson = JSON.parse(fs.readFileSync(path.resolve(resolvedRoot, 'package.json'), 'utf8'))
-
-    if (packageJson.dependencies) {
-      external.push(...Object.keys(packageJson.dependencies))
-    }
-
-    // if (packageJson.devDependencies) {
-    //   external.push(...Object.keys(packageJson.devDependencies))
-    // }
+export function resolveElectronConfig(config: ResolvedElectronOptions, extra?: ElectronBuildConfig): InlineConfig {
+  if (!config.main) {
+    throw new Error(`armonia-electron: electron.main is not defined`)
   }
 
-  const buildConfig = mergeConfig(config, {
-    configFile: false, // TODO: we do not allow the user to reference a config file
+  // NOTE: check: rollupOptions.output.entryFileNames
+  const main = `${path.parse(config.main).name}.js`
+  const preload = config.preload ? `./${path.parse(config.preload).name}.js` : undefined
+
+  const electronConfig: InlineConfig = {
+    configFile: false,
     publicDir: false,
+    root: config.root,
+    mode: config.mode,
+
+    // ok what is this?
+    customLogger: config._resolvedConfig.logger,
 
     define: {
       'import.meta.env.ELECTRON': true,
-      'import.meta.env.ELECTRON_APP_URL': JSON.stringify(appUrl),
-      'import.meta.env.ELECTRON_PRELOAD_URL': preloadUrl ? JSON.stringify(preloadUrl) : 'undefined'
+      'import.meta.env.ELECTRON_APP_URL': extra?.url ? JSON.stringify(extra?.url) : JSON.stringify(main),
+      'import.meta.env.ELECTRON_PRELOAD_URL': preload ? JSON.stringify(preload) : 'undefined'
     },
 
     build: {
-      rollupOptions: {
-        input: preloadEntry ? [mainEntry, preloadEntry] : [mainEntry],
+      outDir: config.outDir,
+      ssr: true,
 
+      // TODO: this should be configurable somehow
+      // minify: false,
+
+      rollupOptions: {
+        input: config.preload ? [config.main, config.preload] : [config.main],
+
+        // TODO: this should be configurable somehow
         output: {
           format: 'cjs',
           entryFileNames: '[name].js',
           chunkFileNames: '[name].js',
           assetFileNames: '[name].[ext]'
-        },
-
-        // TODO: this looks more like an hack than an actual smart way to go about it
-        external: [
-          ...external,
-
-          'electron',
-
-          // node core modules
-          'assert',
-          'async_hooks',
-          'buffer',
-          'child_process',
-          'cluster',
-          'console',
-          'crypto',
-          'dgram',
-          'dns',
-          'events',
-          'fs',
-          'fs/promises',
-          'http',
-          'http2',
-          'https',
-          'inspector',
-          'module',
-          'net',
-          'os',
-          'path',
-          'perf_hooks',
-          'process',
-          'querystring',
-          'readline',
-          'repl',
-          'stream',
-          'string_decoder',
-          'timers',
-          'tls',
-          'tty',
-          'url',
-          'util',
-          'v8',
-          'vm',
-          'wasi',
-          'worker',
-          'zlib',
-
-          // for node 16
-          'node:assert',
-          'node:async_hooks',
-          'node:buffer',
-          'node:child_process',
-          'node:cluster',
-          'node:console',
-          'node:crypto',
-          'node:dgram',
-          'node:dns',
-          'node:events',
-          'node:fs',
-          'node:fs/promises',
-          'node:http',
-          'node:http2',
-          'node:https',
-          'node:inspector',
-          'node:module',
-          'node:net',
-          'node:os',
-          'node:path',
-          'node:perf_hooks',
-          'node:process',
-          'node:querystring',
-          'node:readline',
-          'node:repl',
-          'node:stream',
-          'node:string_decoder',
-          'node:timers',
-          'node:tls',
-          'node:tty',
-          'node:url',
-          'node:util',
-          'node:v8',
-          'node:vm',
-          'node:wasi',
-          'node:worker',
-          'node:zlib'
-        ]
+        }
       }
     },
 
     plugins: [
-      // {
-      //   name: 'electron-external',
-      //   resolveId(source, importer) {
-      //     if (importer && ['.', '/', '\\'].some((id) => source.startsWith(id))) {
-      //       return {
-      //         id: source,
-      //         external: true,
-      //         moduleSideEffects: true
-      //       }
-      //     }
-
-      //     return null
-      //   }
-      // },
-      {
-        name: 'electron-resolve-package-json',
-
-        buildStart() {
-          this.addWatchFile('./package.json')
-        },
-
-        async buildEnd() {
-          // TODO: provide a way to resolve package.json manually
-          const resolved = await this.resolve('./package.json')
-
-          let pkg: any = {}
-
-          if (resolved) {
-            try {
-              // TODO: get rid of `fs`, as right now, this is not possible
-              if (fs.existsSync(resolved.id)) {
-                pkg = JSON.parse(fs.readFileSync(resolved.id, 'utf8'))
-              }
-            } catch (error) {
-              const message = 'Could not parse package.json file'
-              this.warn({ message, id: './package.json' })
-            }
-          }
-
-          pkg.main = mainName
-
-          // use this as a configuration object instead
-          // const configDependencies: string[] | Record<string, string> = {}
-
-          // HACK: this is wrong
-          delete pkg.dependencies.vue
-          delete pkg.dependencies['vue-router']
-          delete pkg.dependencies['vuex']
-          delete pkg.dependencies['react']
-          delete pkg.dependencies['redux']
-
-          // HACK: this is wrong, its a fix for electron-builder
-          if (pkg.devDependencies?.electron) {
-            pkg.devDependencies.electron = trim(pkg.devDependencies.electron, '^')
-          }
-
-          if (options?.transformPackageJson) {
-            await options?.transformPackageJson(pkg)
-          }
-
-          // emit the file
-          this.emitFile({
-            type: 'asset',
-            name: 'package.json',
-            source: JSON.stringify(pkg, null, 2) // TODO: allow customizations? the dev could write a plugin for cleanup
-          })
-        }
-      }
+      emitPackageJsonPlugin(config, (pkg) => {
+        pkg.main = main
+      })
     ]
-  } as InlineConfig)
+  }
 
-  return await viteBuild(buildConfig)
+  return mergeConfig(config.electronConfig || {}, electronConfig)
 }
--- packages/armonia-vite/src/plugin-electron/index.ts
+++ packages/armonia-vite/src/plugin-electron/index.ts
@@ -3,13 +3,13 @@ import type { Options as ElectronPackagerConfig } from 'electron-packager'
 import fs from 'fs'
 import path from 'path'
 import type { RollupWatcher } from 'rollup'
-import type { ConfigEnv, Plugin, ResolvedConfig, UserConfig, ViteDevServer } from 'vite'
-import { mergeConfig } from 'vite'
+import type { ConfigEnv, Plugin, ResolvedConfig, SSROptions, UserConfig, ViteDevServer } from 'vite'
+import { build, loadConfigFromFile, mergeConfig, normalizePath } from 'vite'
 
 import { emitBundle } from '../common/emit_bundle'
 import { ok, warn } from '../common/log'
 import { resolveAddress } from '../common/resolve_address'
-import { buildElectron } from './build'
+import { resolveElectronConfig } from './build'
 import type { ElectronProcess } from './electron_process'
 import { runElectron } from './electron_process'
 
@@ -23,7 +23,7 @@ export interface ElectronOptions {
   argv?: string[]
 
   /**
-   * Defines the electron main file.
+   * Defines the electron main file, relative to the project root.
    *
    * @note Omit the file extension to resolve `.js` and `.ts` automatically
    *
@@ -43,7 +43,7 @@ export interface ElectronOptions {
   main?: string
 
   /**
-   * Defines the electron preload file.
+   * Defines the electron preload file, relative to the project root.
    *
    * @note Omit the file extension to resolve `.js` and `.ts` automatically
    *
@@ -83,7 +83,7 @@ export interface ElectronOptions {
   /**
    * Dependencies to always exclude.
    */
-  excludeDependencies?: string[]
+  excludeDependencies?: string[] | Record<string, string>
 
   /**
    * The package.json file location relative the the vite project root.
@@ -99,27 +99,33 @@ export interface ElectronOptions {
    * Overwrite the vite config.
    */
   config?: UserConfig
+
+  /**
+   * Overwrite the vite config for electron.
+   */
+  electronConfig?: UserConfig & {
+    ssr?: SSROptions
+  }
 }
 
 type ElectronPackager = (opts: ElectronPackagerConfig) => Promise<string[]>
 
-function createRunner(options?: ElectronOptions) {
+function createManager() {
   let electron: ElectronProcess | undefined
   let watcher: RollupWatcher | undefined
 
   return {
-    async run(server: ViteDevServer) {
-      const resolvedConfig = server.config
+    async run(server: ViteDevServer, options: ResolvedElectronOptions) {
+      const url = resolveAddress(server).href
 
-      watcher = (await buildElectron(
-        {
-          configFile: false, // do not load the config file
-          mode: resolvedConfig.mode,
-          root: resolvedConfig.root,
+      const config = resolveElectronConfig(options, {
+        url
+      })
+
+      watcher = (await build(
+        mergeConfig(config, {
           build: {
-            watch: {},
-            minify: false,
-            outDir: resolvedConfig.build.outDir
+            watch: {}
           },
           plugins: [
             {
@@ -127,15 +133,15 @@ function createRunner(options?: ElectronOptions) {
               async closeBundle() {
                 const el = electron
                 electron = undefined
+
+                ok(server.config.logger, 'restarting electron...')
+
                 await el?.close()
-                electron = await runElectron(resolvedConfig.build.outDir, options?.argv)
+                electron = await runElectron(options.outDir, options?.argv)
               }
             }
           ]
-        },
-        {
-          url: resolveAddress(server).href
-        }
+        })
       )) as RollupWatcher
     },
     async close() {
@@ -155,16 +161,162 @@ function createRunner(options?: ElectronOptions) {
 // ElectronPlatformName = "darwin" | "linux" | "win32" | "mas";
 // ArchType = "x64" | "ia32" | "armv7l" | "arm64" | "universal";
 
+function resolveFileEntry(root: string, file: string | undefined, locations: string[]): string | false {
+  if (file) {
+    locations = [file]
+  }
+
+  for (const location of locations) {
+    // get the full path
+    const fileName = path.resolve(root, location)
+
+    // get the file extension, if any
+    const { ext } = path.parse(fileName)
+
+    // no extension, guess it
+    if (ext === '') {
+      // typescript
+      if (fs.existsSync(`${fileName}.ts`)) {
+        return path.relative(root, `${fileName}.ts`)
+      }
+
+      // javascript
+      if (fs.existsSync(`${fileName}.js`)) {
+        return path.relative(root, `${fileName}.js`)
+      }
+    } else {
+      if (fs.existsSync(fileName)) {
+        return path.relative(root, fileName)
+      }
+    }
+  }
+
+  return false
+}
+
+export interface ResolvedElectronOptions extends ElectronOptions {
+  root: string
+  outDir: string
+  mode: ConfigEnv['mode']
+  /** @private  */
+  _resolvedConfig: ResolvedConfig
+}
+
+async function resolveElectronOptions(
+  env: ConfigEnv,
+  resolvedConfig: ResolvedConfig,
+  options?: ElectronOptions
+): Promise<ResolvedElectronOptions> {
+  const root = resolvedConfig.root
+
+  let main =
+    resolveFileEntry(root, options?.main, [
+      'electron/main',
+      'electron/index',
+      'electron/electron',
+      //
+      'src-electron/main',
+      'src-electron/index',
+      'src-electron/electron',
+      'src-electron/electron-main',
+      //
+      'src/electron',
+      'src/electron-main'
+    ]) || undefined
+
+  if (main) {
+    main = normalizePath(path.relative(root, path.resolve(root, main)))
+  }
+
+  let preload =
+    resolveFileEntry(root, options?.preload, [
+      'electron/preload.ts',
+      'electron/preload.js',
+      //
+      'src-electron/preload.ts',
+      'src-electron/preload.js',
+      'src-electron/electron-preload.ts',
+      'src-electron/electron-preload.js',
+      //
+      'src/preload.ts',
+      'src/preload.js',
+      'src/electron-preload.ts',
+      'src/electron-preload.js'
+    ]) || undefined
+
+  if (preload) {
+    preload = normalizePath(path.relative(root, path.resolve(root, preload)))
+  }
+
+  // package.json
+  const packageJsonFile = path.resolve(root, options?.packageJson || 'package.json')
+  let packageJson = fs.existsSync(packageJsonFile) ? packageJsonFile : undefined
+
+  if (packageJson) {
+    packageJson = normalizePath(path.relative(root, path.resolve(root, packageJson)))
+  }
+
+  // user config
+  let config = options?.config
+
+  if (typeof config === 'string') {
+    const overwriteConfig = await loadConfigFromFile(env, config, undefined, resolvedConfig.logLevel)
+
+    config = overwriteConfig ? overwriteConfig.config : undefined
+  }
+
+  // bundler
+  let bundler = options?.bundler
+
+  if ((bundler === undefined || bundler == null) && packageJson) {
+    // TODO: consider using: require.resolve('electron-builder') instead of package.json
+    const jsonPackage = JSON.parse(fs.readFileSync(path.resolve(root, packageJson), 'utf8'))
+
+    if (jsonPackage.devDependencies && jsonPackage.devDependencies['electron-builder']) {
+      bundler = 'builder'
+    } else if (jsonPackage.devDependencies && jsonPackage.devDependencies['electron-packager']) {
+      bundler = 'packager'
+    }
+  }
+
+  const resolvedElectronOptions: ResolvedElectronOptions = {
+    root,
+    outDir: resolvedConfig.build.outDir,
+    mode: resolvedConfig.mode,
+    // this field will be removed
+    _resolvedConfig: resolvedConfig
+  }
+
+  // this is temporary, i want to rewrite this piece of code eventually
+  return Object.assign(resolvedElectronOptions, {
+    main,
+    preload,
+    packageJson,
+    config,
+    bundler,
+    builder: options?.builder,
+    packager: options?.packager,
+    dependencies: options?.dependencies,
+    excludeDependencies: options?.excludeDependencies,
+    transformPackageJson: options?.transformPackageJson,
+    electronConfig: options?.electronConfig,
+    argv: options?.argv
+  }) as ResolvedElectronOptions
+}
+
 export default function electron(options?: ElectronOptions): Plugin {
-  const electronManager = createRunner(options)
+  const electronManager = createManager()
   let resolvedConfig: ResolvedConfig
+  let resolvedElectronOptions: ResolvedElectronOptions
   let command: ConfigEnv['command']
+  let env: ConfigEnv
 
   return {
-    name: 'vite-plugin-armonia-electron',
+    name: 'armonia:vite-plugin-electron',
 
-    config(_, env) {
-      command = env.command
+    config(_, envConfig) {
+      env = envConfig
+      command = envConfig.command
 
       const electronConfig: UserConfig = {
         define: {
@@ -180,8 +332,16 @@ export default function electron(options?: ElectronOptions): Plugin {
       return mergeConfig(options?.config || {}, electronConfig)
     },
 
-    configResolved(config) {
+    async configResolved(config) {
       resolvedConfig = config
+
+      // resolve the electron options
+      resolvedElectronOptions = await resolveElectronOptions(env, config)
+
+      // throw if we do not have a main input file
+      if (!resolvedElectronOptions.main) {
+        throw new Error(`armonia-electron: electron.main is not defined`)
+      }
 
       // electron resolve the path relative to the binary location
       // vite build uses '/' which will result in electron loading index.html and assets from the OS root
@@ -200,7 +360,7 @@ export default function electron(options?: ElectronOptions): Plugin {
       }
 
       server.httpServer.on('listening', () => {
-        electronManager.run(server).catch((error) => {
+        electronManager.run(server, resolvedElectronOptions).catch((error) => {
           server.config.logger.error(error)
         })
       })
@@ -220,19 +380,14 @@ export default function electron(options?: ElectronOptions): Plugin {
       // notify we are building electron
       ok(resolvedConfig.logger, `building electron assets for ${resolvedConfig.mode}...`)
 
-      const bundle = await buildElectron(
-        {
-          configFile: false, // do not load the config file
-          mode: resolvedConfig.mode,
-          root: resolvedConfig.root,
+      const electronBuildConfig = resolveElectronConfig(resolvedElectronOptions)
+
+      const bundle = await build(
+        mergeConfig(electronBuildConfig, {
           build: {
-            minify: false,
-            write: false, // emit manually
-            outDir: resolvedConfig.build.outDir
+            write: false // emit manually
           }
-        },
-        undefined,
-        options
+        })
       )
 
       emitBundle(this, bundle)
@@ -242,34 +397,20 @@ export default function electron(options?: ElectronOptions): Plugin {
     async closeBundle() {
       await electronManager.close()
 
+      // forcefully bundle only on build
       if (command !== 'build') {
         return
       }
 
-      if (options?.bundler === false) {
+      // bundling is disabled
+      if (resolvedElectronOptions?.bundler === false) {
         return
       }
 
-      const dir = path.resolve(resolvedConfig.root, resolvedConfig.build.outDir)
-      const out = path.resolve(resolvedConfig.root, resolvedConfig.build.outDir, 'dist')
+      const dir = path.resolve(resolvedElectronOptions.root, resolvedElectronOptions.outDir)
+      const out = path.resolve(resolvedElectronOptions.root, resolvedElectronOptions.outDir, 'dist')
 
-      let packageJson: Record<string, any> = {}
-
-      const packageJsonFile = path.resolve(resolvedConfig.root, options?.packageJson || './package.json')
-
-      if (fs.existsSync(packageJsonFile)) {
-        packageJson = JSON.parse(fs.readFileSync(packageJsonFile, 'utf8'))
-      }
-
-      let bundler = options?.bundler
-
-      if (typeof bundler === 'undefined' || bundler == null) {
-        if (packageJson.devDependencies && packageJson.devDependencies['electron-builder']) {
-          bundler = 'builder'
-        } else if (packageJson.devDependencies && packageJson.devDependencies['electron-packager']) {
-          bundler = 'packager'
-        }
-      }
+      const bundler = resolvedElectronOptions.bundler
 
       if (bundler === 'builder') {
         // notify we are bundling electron
