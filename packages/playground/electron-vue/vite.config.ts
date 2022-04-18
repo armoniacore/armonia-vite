3a1880eee338ddad9119415331ecf429e938a336
lucacicada <100947658+lucacicada@users.noreply.github.com>
2022-04-18 10:55:37 +0200

    use electron plugin

--- packages/playground/electron-vue/vite.config.ts
+++ packages/playground/electron-vue/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { armonia } from '@armonia/vite'
+import { electron } from '@armonia/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), armonia({ target: 'electron' })]
+  plugins: [vue(), electron()]
 })
